9ba7405cd64207a6284d3c166662458a6b34adf9
AutumnKennedy <autumnkennedy0714@gmail.com>
2024-06-03 21:44:56 -0500

    Created models

---
+++ ChatterBoxAPI/Features/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatterBoxAPI.Features.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
