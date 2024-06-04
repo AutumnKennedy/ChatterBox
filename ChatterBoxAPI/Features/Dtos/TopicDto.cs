12e2535690f7c1986079b32f1ef2434281d100a4
AutumnKennedy <autumnkennedy0714@gmail.com>
2024-06-03 21:45:31 -0500

    created Dtos

---
+++ ChatterBoxAPI/Features/Dtos/TopicDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatterBoxAPI.Features.Dtos
+{
+    public class TopicDto
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Title { get; set; }
+    }
+}
